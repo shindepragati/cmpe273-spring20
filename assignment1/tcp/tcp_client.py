de10d0e25e1aa95066bf396bd302075b25ac134e
shindepragati <60525827+shindepragati@users.noreply.github.com>
2020-03-05 12:15:56 -0800

    Add files via upload

--- assignment1/tcp/tcp_client.py
+++ assignment1/tcp/tcp_client.py
@@ -1,23 +1,54 @@
 import socket
-
+import sys
+import time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
-BUFFER_SIZE = 1024
+BUFFER_SIZE1 = 1024
+BUFFER_SIZE2 = 1024
 MESSAGE = "ping"
 
-def send(id=0):
+
+def send():
+    id=sys.argv[1]
+    delay=sys.argv[2]
+    msgcount=sys.argv[3]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(f"{id}:{MESSAGE}".encode())
-    data = s.recv(BUFFER_SIZE)
-    s.close()
-    print("received data:", data.decode())
+    
+    count = 1
+    while True:
+        if(count <= int(msgcount)):
+            print("sending data:", MESSAGE)
+            s.send(f"{id}:{MESSAGE}".encode())
+            data = s.recv(BUFFER_SIZE1)
+            print("received data:", data.decode())
+            count += 1
+            time.sleep(int(delay))
+        else:
+           print("done bye")
+           s.close()
+           break 
+    
 
 
-def get_client_id():
-    id = input("Enter client id:")
-    return id
+send()
 
 
-send(get_client_id())
+#def main():
+    # creating thread 
+   # reloader=False
+   # t1 = threading.Thread(target=tcp_client1)
+    #t2 = threading.Thread(target=tcp_client2) 
+  
+    # Start new Threads
+   # t1.start()
+   # t2.start()
+    # wait until thread 1 is completely executed 
+   # t1.join() 
+    # wait until thread 2 is completely executed 
+   # t2.join() 
+
+#if __name__ == "__main__": 
+ #   main()
+ 
--- assignment1/tcp/tcp_server.py
+++ assignment1/tcp/tcp_server.py
@@ -1,26 +1,41 @@
 import socket
+import threading 
 
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
+clients={}
 
-def listen_forever():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
 
-    conn, addr = s.accept()
-    print(f'Connection address:{addr}')
+client_lock = threading.Lock() 
 
+def listen_forever(conn,addr):
     while True:
         data = conn.recv(BUFFER_SIZE)
         if not data: 
-            print('No data received.')
-            break
+           print('No data received.')
+           client_lock.release() 
+           break
         print(f"received data:{data.decode()}")
         conn.send("pong".encode())
 
     conn.close()
 
-listen_forever()
+
+def Main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(2)
+    print(f'Server started at port:{TCP_PORT}')
+    while True: 
+        conn, addr = s.accept() 
+        client_lock.acquire() 
+        print('Connected to :', addr[0], ':', addr[1]) 
+  
+        threading._start_new_thread(listen_forever, (conn,addr)) 
+    s.close() 
+  
+  
+if __name__ == '__main__': 
+    Main() 
