40f03f34b7ee6f9f0f84e72b0093f674cba0e392
shindepragati <60525827+shindepragati@users.noreply.github.com>
2020-03-05 00:47:47 -0800

    Add files via upload

--- assignment1/udp/udp_client.py
+++ assignment1/udp/udp_client.py
@@ -1,25 +1,71 @@
 import socket
-
+import time
 
 UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
 BUFFER_SIZE = 1024
-MESSAGE = "ping"
+MESSAGE = "ping";
+send_againFlag = 0
+prev_line=''
+prev_id=0
+count = 0
 
-
-def send(id=0):
+def send(filename):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto(f"{id}:{MESSAGE}".encode(), (UDP_IP, UDP_PORT))
-        data, ip = s.recvfrom(BUFFER_SIZE)
-        print("received data: {}: {}".format(ip, data.decode()))
+        
+        print("Connected to the server.")
+        fileinput = open(filename, 'r')
+        #with open("udp/upload.txt", 'r') as fileinput:
+        print("Starting a file (upload.txt) upload...")
+        for line in fileinput:
+            s.settimeout(1.0)
+            global prev_line,prev_id
+            #print("lineee",line)
+            prev_line=line
+            prev_id=prev_id+1
+            try:
+                s.sendto(f"{prev_id}:{prev_line}".encode(), (UDP_IP, UDP_PORT))
+                data, ip = s.recvfrom(BUFFER_SIZE)
+                printRespose(data)
+            except socket.timeout:
+                resend()
+            
+        print("File upload successfully completed.")    
+        s.close()
+
     except socket.error:
         print("Error! {}".format(socket.error))
-        exit()
+      
+def resend():
+    global count
+    count += 1
+    while count <= 5:
+        
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.settimeout(1.0)
+            s.sendto(f"{prev_id}:{prev_line}".encode(), (UDP_IP, UDP_PORT))
+            print("Lost packet",prev_id)
+            data, ip = s.recvfrom(BUFFER_SIZE)  
+            if(len(data)==0 ):
+                count += 1
+            else:
+                print(data)
+                #send()
+                break
+        except socket.timeout:
+            resend()
+       
+
+def printRespose(data):
+    global prev_id
+    temp=prev_id
+    temp=temp*2
+    if(len(data) > 0 and str(temp)==data.decode()):
+        print("Received ack(",data.decode(),") from the server.")
+
+send("/Users/karthikkathirvel/Desktop/GitHub_273/assignment/assignment1/udp/upload.txt")
 
 
-def get_client_id():
-    id = input("Enter client id:")
-    return id
-
-send(get_client_id())
+    
--- assignment1/udp/udp_server.py
+++ assignment1/udp/udp_server.py
@@ -4,18 +4,31 @@ import socket
 UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
 BUFFER_SIZE = 1024
-MESSAGE = "pong"
+#MESSAGE = "pongg"
 
 def listen_forever():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("", UDP_PORT))
-
-    while True:
-        # get the data sent to us
-        data, ip = s.recvfrom(BUFFER_SIZE)
-        print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
-        # reply back to the client
-        s.sendto(MESSAGE.encode(), ip)
-
+    print("Server started at port",UDP_PORT)
+    print("Accepting a file upload...") 
+    try:
+        while True:
+            # get the data sent to us
+            data, ip = s.recvfrom(BUFFER_SIZE)
+          
+            rec_str = data.decode(encoding="utf-8").strip()  
+            str1=rec_str.split(':',1)
+            temp=int(str1[0])*2                 
+            temp=str(temp)
+            #print("------"+str1[1])
+            if(str1[1].startswith("9987")):   #example of lost packet
+                temp=""
+            else:
+                s.sendto(temp.encode(), ip)
+            if(temp=="20000"):
+                print("Upload successfully completed.")
+    except Exception:
+        print("Error!",Exception)
+        exit()
 
 listen_forever()
